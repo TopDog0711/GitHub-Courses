3bacb8e53e137721535c6bc84541d70df416e5cd
TopDog0711 <horn.michael.j@gmail.com>
2014-11-12 13:54:19 -0700

    Added unit tests.. all done

--- Courses/Courses/UnitTestProject1/UnitTest1.cs
+++ Courses/Courses/UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
@@ -7,9 +10,55 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TokenTest()
+        public async Task TokenTest()
         {
-            
+            try
+            {
+                string token = await CoursesApi.GetToken();
+                Assert.IsNotNull(token);
+
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+                
+            }
+        }
+
+        [TestMethod]
+        public async Task CoursesTest()
+        {
+            try
+            {
+                string token = await CoursesApi.GetToken();
+                List<Course> courses = await CoursesApi.GetCourses(token);
+                Assert.IsNotNull(courses);
+                Assert.IsTrue(courses.Count > 0);
+
+
+
+            }
+            catch(Exception)
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        [TestMethod]
+        public async Task SingleCourseTest()
+        {
+            try
+            {
+                string token = await CoursesApi.GetToken();
+                Course course = await CoursesApi.GetCourseById(token, 1);
+                Assert.IsNotNull(course);
+                
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
         }
     }
 }
